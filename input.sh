149c2ffba3aada2ee9e29129e574c4c85060b1c2
Cerotow <sansanime31@gmail.com>
2024-12-17 04:25:05 +0800

    Update input.sh

--- input.sh
+++ input.sh
@@ -4,4 +4,4 @@ export APP_ID=105600
 export ENTRY_ID=2439880423
 
 # tmod file to extract using extractTmod.yml workflow
-export TMODFILE="EquipAllAccessory_v0.0.0.3.tmod"
+export TMODFILE="BIntros.tmod"
