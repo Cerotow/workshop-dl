1e08b86533e742f111facf017d948db755a44fac
Cerotow <sansanime31@gmail.com>
2026-02-05 02:37:19 +0800

    Update input.sh

--- input.sh
+++ input.sh
@@ -4,4 +4,4 @@ export APP_ID=1281930
 export ENTRY_ID=3018274567
 
 # tmod file to extract using extractTmod.yml workflow
-export TMODFILE="UpgradedAccessories.tmod"
+export TMODFILE="ArtificerMod.tmod"
