430dfe4746973c2d82c03002a896c032df8a84fa
Cerotow <sansanime31@gmail.com>
2026-02-08 05:45:23 +0800

    Update tModUnpacker.bat

--- tModUnpacker.bat
+++ tModUnpacker.bat
@@ -6,6 +6,9 @@ SET TMODFILE=MEAC
 mkdir "tmodFileExtracted\\%TMODFILE%"
 tModUnpacker.exe "downloaded\\2025.3\\%TMODFILE%.tmod" "tmodFileExtracted\\%TMODFILE%\\"
 
+powershell -Command ^
+  "Compress-Archive -Path 'tmodFileExtracted\%TMODFILE%\*' -DestinationPath '%TMODFILE%.zip' -Force"
+
 git config user.name "github-actions[bot]"
 git config user.email "github-actions[bot]@users.noreply.github.com"
 git add "tmodFileExtracted\\%TMODFILE%\\"
