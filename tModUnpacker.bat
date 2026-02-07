d8473e2f40290037d05d63db17e13724678a4b5e
Cerotow <sansanime31@gmail.com>
2026-02-08 05:52:42 +0800

    Update tModUnpacker.bat

--- tModUnpacker.bat
+++ tModUnpacker.bat
@@ -12,6 +12,6 @@ powershell -Command ^
 git config user.name "github-actions[bot]"
 git config user.email "github-actions[bot]@users.noreply.github.com"
 git add "tmodFileExtracted\\%TMODFILE%\\"
-git add "%TMODFILE%.zip"
+git add "tmodFileExtracted\\%TMODFILE%\\%TMODFILE%.zip"
 git commit -m "Extracted %TMODFILE%.tmod"
 git push
