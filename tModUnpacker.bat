18985b752b1ac4ebf0185dde58a474c181a75eb8
Cerotow <sansanime31@gmail.com>
2026-02-08 05:31:25 +0800

    Update tModUnpacker.bat

--- tModUnpacker.bat
+++ tModUnpacker.bat
@@ -1,5 +1,5 @@
 :: Do not use double quote here
-SET TMODFILE=ArtificerMod
+SET TMODFILE=MEAC
 
 :: -------
 
@@ -8,6 +8,7 @@ tModUnpacker.exe "tmodFile\\%TMODFILE%.tmod" "tmodFileExtracted\\%TMODFILE%\\"
 
 git config user.name "github-actions[bot]"
 git config user.email "github-actions[bot]@users.noreply.github.com"
-git add "tmodFileExtracted\\%TMODFILE%\\"
+:: git add "tmodFileExtracted\\%TMODFILE%\\"
+git add "downloaded\\2025.3\\%TMODFILE%\\" 
 git commit -m "Extracted %TMODFILE%.tmod"
 git push
