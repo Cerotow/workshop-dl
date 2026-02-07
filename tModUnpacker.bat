870957dc0248f0873c55c6fdc8ed614abcd36933
Cerotow <sansanime31@gmail.com>
2026-02-08 05:39:51 +0800

    Update tModUnpacker.bat

--- tModUnpacker.bat
+++ tModUnpacker.bat
@@ -3,8 +3,8 @@ SET TMODFILE=MEAC
 
 :: -------
 
-mkdir "downloaded\\2025.3\\%TMODFILE%\\"
-tModUnpacker.exe "downloaded\\2025.3\\%TMODFILE%\\" "tmodFileExtracted\\%TMODFILE%\\"
+mkdir "tmodFileExtracted\\%TMODFILE%"
+tModUnpacker.exe "downloaded\\2025.3\\%TMODFILE%.tmod\\" "tmodFileExtracted\\%TMODFILE%\\"
 
 git config user.name "github-actions[bot]"
 git config user.email "github-actions[bot]@users.noreply.github.com"
