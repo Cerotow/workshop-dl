b01b4221447b5c6c85102de7f653cc61e0699aa5
Cerotow <sansanime31@gmail.com>
2026-02-10 10:56:23 +0800

    Update input.sh

--- input.sh
+++ input.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # 105600 for Terraria, 1281930 for TModLoader
 export APP_ID=1281930
-export ENTRY_ID=2829306780
+export ENTRY_ID=2815777479
 
 # tmod file to extract using extractTmod.yml workflow
 export TMODFILE="CoolerItemVisualEffect.tmod"
