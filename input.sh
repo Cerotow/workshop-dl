51379016178ae4985586cc1443bc1e02f394cfb5
Cerotow <sansanime31@gmail.com>
2026-02-16 01:56:01 +0800

    Update input.sh

--- input.sh
+++ input.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # 105600 for Terraria, 1281930 for TModLoader
 export APP_ID=1281930
-export ENTRY_ID=2815777479
+export ENTRY_ID=2966457992
 
 # tmod file to extract using extractTmod.yml workflow
 export TMODFILE="DDmod.tmod"
