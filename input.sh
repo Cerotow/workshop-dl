5f7b136f1c8906845057369aaa0248e2f700aa7b
Cerotow <sansanime31@gmail.com>
2026-02-17 22:40:15 +0800

    Update input.sh

--- input.sh
+++ input.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # 105600 for Terraria, 1281930 for TModLoader
 export APP_ID=1281930
-export ENTRY_ID=2966457992
+export ENTRY_ID=3645731835
 
 # tmod file to extract using extractTmod.yml workflow
 export TMODFILE="MeleeEffects.tmod"
